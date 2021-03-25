d4983cb5f70f4d5770e0702a621b7e22543ec0b2
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-25 08:53:44 +0530

    Project country: country code: should not be null

--- Spring REST HOL 004/restapi-country/src/main/java/com/cognizant/restapicountry/model/Country.java
+++ Spring REST HOL 004/restapi-country/src/main/java/com/cognizant/restapicountry/model/Country.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class Country {
 
 	// properties
-	@NotNull
+	@NotNull(message = "Country code should not be null")
 	@Size(min = 2, max = 2, message = "Country code should be 2 characters")
 	private String code;
 	private String name;
