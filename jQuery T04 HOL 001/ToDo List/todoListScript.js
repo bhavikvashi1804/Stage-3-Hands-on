0b656af701140b1ea19cb76aa3748c0a3a9f0954
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-26 14:43:58 +0530

    jQuery T04 HOL 001 Done

--- jQuery T04 HOL 001/ToDo List/todoListScript.js
+++ jQuery T04 HOL 001/ToDo List/todoListScript.js
@@ -3,6 +3,11 @@ $(document).ready(() => {
     let taskName = $("#taskName").val();
     if (taskName == "") {
       alert("Enter task details!!");
+    } else {
+      //append the task to Task list
+      $("#taskList").append("<br>" + taskName);
+      //clear the userInput field
+      $("#taskName").val("");
     }
   });
 });
