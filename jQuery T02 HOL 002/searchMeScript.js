b901e41cfcdfc7c34f5f964f07fddde7170ed5ce
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-26 09:55:40 +0530

    Search the term: still one error

--- jQuery T02 HOL 002/searchMeScript.js
+++ jQuery T02 HOL 002/searchMeScript.js
@@ -10,9 +10,21 @@ $(document).ready(function () {
       }
     );
 
-  $(".searchWord").css({
-    "font-style": "italic",
-    "text-shadow": "2px 2px pink",
-    "font-weight": "bold",
+  $("input").change(function () {
+    var searchTerm = $(this).val();
+    //console.log(text);
+    $("#par")
+      .children("span")
+      .each(function () {
+        var spanText = $(this).text();
+
+        if (spanText.indexOf(searchTerm) != -1) {
+          $(this).css({
+            "font-style": "italic",
+            "text-shadow": "2px 2px pink",
+            "font-weight": "bold",
+          });
+        }
+      });
   });
 });
