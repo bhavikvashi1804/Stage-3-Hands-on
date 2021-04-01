a56b293d94284afe07b068f5e458116b0b7149da
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-01 10:42:16 +0530

    provide the dependency of department into the Employee interface

--- Angular T04 HOL 001/Employee/employee-test.js
+++ Angular T04 HOL 001/Employee/employee-test.js
@@ -1,12 +1,19 @@
 "use strict";
 exports.__esModule = true;
+var payrollDept = {
+    id: 1,
+    name: "Payroll"
+};
 var ctsEmployee = {
     id: 100,
     name: "Raj Patel",
     salary: 100000,
-    permanent: false
+    permanent: false,
+    department: payrollDept
 };
 console.log("CTS Employee Details");
-console.log("Employee ID: " + ctsEmployee.id);
-console.log("Employee Name: " + ctsEmployee.name);
-console.log("Employee ID: " + ctsEmployee.salary);
+console.log("ID: " + ctsEmployee.id);
+console.log("Name: " + ctsEmployee.name);
+console.log("Salary: " + ctsEmployee.salary);
+console.log("Department ID: " + ctsEmployee.department.id);
+console.log("Department Name: " + ctsEmployee.department.name);
--- Angular T04 HOL 001/Employee/employee-test.ts
+++ Angular T04 HOL 001/Employee/employee-test.ts
@@ -1,15 +1,22 @@
 import { Employee } from "./employee";
+import { Department } from "./department";
+
+let payrollDept: Department = {
+  id: 1,
+  name: "Payroll",
+};
 
 let ctsEmployee: Employee = {
   id: 100,
   name: "Raj Patel",
   salary: 100000,
   permanent: false,
+  department: payrollDept,
 };
 
-
 console.log("CTS Employee Details");
-console.log("Employee ID: "+ ctsEmployee.id);
-console.log("Employee Name: "+ ctsEmployee.name);
-console.log("Employee ID: "+ ctsEmployee.salary);
-
+console.log("ID: " + ctsEmployee.id);
+console.log("Name: " + ctsEmployee.name);
+console.log("Salary: " + ctsEmployee.salary);
+console.log("Department ID: " + ctsEmployee.department.id);
+console.log("Department Name: " + ctsEmployee.department.name);
