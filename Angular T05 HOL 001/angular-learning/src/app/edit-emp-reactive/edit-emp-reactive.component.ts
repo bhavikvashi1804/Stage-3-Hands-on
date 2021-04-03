b9b1028a928537cb681f11e5dae566c8c276995e
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-03 22:13:13 +0530

    Import reactive forms module

--- Angular T05 HOL 001/angular-learning/src/app/edit-emp-reactive/edit-emp-reactive.component.ts
+++ Angular T05 HOL 001/angular-learning/src/app/edit-emp-reactive/edit-emp-reactive.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Employee } from '../Employee/employee';
 
 @Component({
   selector: 'app-edit-emp-reactive',
   templateUrl: './edit-emp-reactive.component.html',
-  styleUrls: ['./edit-emp-reactive.component.css']
+  styleUrls: ['./edit-emp-reactive.component.css'],
 })
 export class EditEmpReactiveComponent implements OnInit {
+ 
+  //create a new employee property
+  employee: Employee = {
+    id: undefined,
+    name: undefined,
+    salary: undefined,
+    permanent: undefined,
+    department: undefined,
+    skills: undefined,
+  };
 
-  constructor() { }
+  name = new FormControl(this.employee.name);
 
-  ngOnInit(): void {
-  }
+  
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
