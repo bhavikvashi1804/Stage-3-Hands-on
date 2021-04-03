da79a044e13c336931aaa5bfd3fa94e25720253a
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-03 21:47:37 +0530

    create a new component

--- Angular T05 HOL 001/angular-learning/src/app/app.module.ts
+++ Angular T05 HOL 001/angular-learning/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditEmpReactiveComponent } from './edit-emp-reactive/edit-emp-reactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditEmpReactiveComponent
   ],
   imports: [
     BrowserModule,
