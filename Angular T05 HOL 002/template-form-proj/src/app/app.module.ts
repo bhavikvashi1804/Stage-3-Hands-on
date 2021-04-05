853314155c8b7ae75c5f5fa646a47bc8858c3561
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-05 20:18:52 +0530

    Create a new component template-driven-form

--- Angular T05 HOL 002/template-form-proj/src/app/app.module.ts
+++ Angular T05 HOL 002/template-form-proj/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TemplateDrivenFormComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
