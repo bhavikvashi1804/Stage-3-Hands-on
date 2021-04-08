cd1275fd7e5828d4505f78794e2d87368a56a3b5
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-08 12:00:47 +0530

    make a subscriber of Service inside the parent component

--- Angular T06 HOL 002/comp-int/src/app/parent/parent.component.ts
+++ Angular T06 HOL 002/comp-int/src/app/parent/parent.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements OnInit, OnDestroy {
+  message: string;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private data: DataService) {}
 
   ngOnInit(): void {
+    this.subscription = this.data.currentMessage.subscribe(
+      (message) => (this.message = message)
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
