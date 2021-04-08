73afcf35f86f1734f020c3c9a8c16ad53cad9eb5
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-08 10:29:19 +0530

    Way 2: Child to Parent: Sharing Data via ViewChild done

--- Angular T06 HOL 001/comp-int/src/app/child/child.component.ts
+++ Angular T06 HOL 001/comp-int/src/app/child/child.component.ts
@@ -2,14 +2,17 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  template: ` Say {{ message }} `,
+  templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
 })
 export class ChildComponent implements OnInit {
   constructor() {}
 
-  @Input("childMessage") 
+  @Input('childMessage')
   message: string;
+
+  //this message1 will passed from child to parent via  @ViewChild("")
+  message1: string = 'I am child. Sending a message to parent';
 
   ngOnInit(): void {}
 }
--- Angular T06 HOL 001/comp-int/src/app/parent/parent.component.ts
+++ Angular T06 HOL 001/comp-int/src/app/parent/parent.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-parent',
-  template: ` <app-child [childMessage]="parentMessage"></app-child> `,
+  templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css'],
 })
-export class ParentComponent implements OnInit {
+export class ParentComponent implements OnInit,AfterViewInit {
   //way 1: Sharing Data via Input
   //new property the we want to pass it from parent to child
   parentMessage = 'message from parent';
+  message: string;
   constructor() {}
 
   //Way 2: Child to Parent: Sharing Data via ViewChild
+  @ViewChild("myChild")
+  child: ChildComponent;
+
+  
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(){
+    this.message = this.child.message1; 
+  }
 }
