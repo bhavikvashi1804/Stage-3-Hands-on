a3206a4fc3e1d9b3674f7f54bf709377a052a125
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-04-08 10:46:34 +0530

    Way 3. Child to Parent: Sharing Data via Output() and EventEmitter Done

--- Angular T06 HOL 001/comp-int/src/app/child/child.component.ts
+++ Angular T06 HOL 001/comp-int/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -11,8 +11,15 @@ export class ChildComponent implements OnInit {
   @Input('childMessage')
   message: string;
 
+  @Output()
+  messageEvent = new EventEmitter<string>();
+
   //this message1 will passed from child to parent via  @ViewChild("")
   message1: string = 'I am child. Sending a message to parent';
 
   ngOnInit(): void {}
+
+  sendMessage() {
+    this.messageEvent.emit('I am child. Sending a message to parent1');
+  }
 }
